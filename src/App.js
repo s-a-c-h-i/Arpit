739034bf1f16a182e7839b777b168c4fd43b9b9a
Saachi <saachi.20223219@mnnit.ac.in>
2023-11-11 17:55:06 +0530

    uploading

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Loginsignup from './components/Loginsignup/Loginsignup'
+
+function App() {
+  return (
+    <div >
+    <Loginsignup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Loginsignup/Loginsignup.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Loginsignup.css'
+import user_icon from '../assets/person.png'
+import email_icon from '../assets/email.png'
+import password_icon from '../assets/password.png'
+import { useState } from 'react';
+const Loginsignup = () => {
+
+  const [action,setAction]=useState("Sign Up")
+
+
+
+
+
+  return (
+    <div className="sbse-bada">
+      <div className="header">
+    <div className="text">{action}</div>
+      <div className="underline"></div>
+
+        </div>
+  
+    <div className="inputs">
+      {action==="Login"?<div></div> :<div className="input">
+            <img src={user_icon} alt=""></img>
+            <input type="text" placeholder='Username'></input>
+           </div>}
+           
+
+           <div className="input">
+            <img src={email_icon} alt=""></img>
+            <input type="email" placeholder='Email'></input>
+           </div>
+
+           <div className="input">
+            <img src={password_icon} alt=""></img>
+            <input type="password" placeholder='Password'></input>
+           </div>
+    </div>
+    {action==='Sign Up'?<div></div>:<div className="forgot">forgot password</div>}
+
+<div className="submit-container">
+    <div className={action==='Login'?"submit":"submit gray"  } onClick={()=>{setAction("Login")}}>Login </div>
+    <div className={action==='Sign Up'?"submit":"submit gray" } onClick={()=>{setAction("Sign Up")}}>Sign-up</div>
+
+</div>
+</div>
+  );
+}
+
+export default Loginsignup
